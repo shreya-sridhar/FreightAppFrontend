5678bd7435450fa7a4a2747e563aa47f928b7340
shreya-sridhar <shreyamohak@outlook.com>
2021-02-24 22:22:07 -0800

    All binaries

---
+++ App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View,Dimensions } from 'react-native';
+// import MapView from 'C:/Users/shrey/DoneWithIt/node_modules/react-native-maps';
+import Home from './pages/home.js';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Home/>
+      <StatusBar style="auto" />
+      {/* <MapView style={styles.map}/> */}
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+   map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
+
+
+
+
