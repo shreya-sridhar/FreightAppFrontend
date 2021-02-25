71f60e9c433d7a13edd4690f65c9543cd330b705
shreya-sridhar <shreyamohak@outlook.com>
2021-02-24 23:24:03 -0800

    Info Bar

--- App.js
+++ App.js
@@ -1,33 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View,Dimensions } from 'react-native';
-import MapView from 'C:/Users/shrey/FreightApp/node_modules/react-native-maps';
-//import Home from './pages/home.js';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import MapView from "C:/Users/shrey/FreightApp/node_modules/react-native-maps";
+import Home from "./pages/home.js";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <Home/> */}
-      {/* <StatusBar style="auto" /> */}
-      <MapView style={styles.map}/>
+      <StatusBar style="auto" />
+      <MapView style={styles.map} />
+      <Home />
     </View>
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
-
-
-
-
--- pages/home.js
+++ pages/home.js
@@ -2,33 +2,29 @@ import React from "react";
 import { Text, View, Image } from "react-native";
 
 // import HomeMap from '../components/HomeMap/index.js';
-import RaspberryStrip from "../components/RaspberryStrip";
-import SearchBar from "../components/SearchBar";
+import RaspberryStrip from "C:/Users/shrey/FreightApp/components/RaspberryStrip.js";
+import SearchBar from "C:/Users/shrey/FreightApp/components/Searchbar.js";
 // import Carousel from "../components/Carousel";
 // import HomeSearch from '../../components/HomeSearch';
-// import taxiimg from './../assets/taxiimg.jpg'; 
+// import taxiimg from './../assets/taxiimg.jpg';
 // import {dummyData} from '../data/Data'
 // import Video from 'react-native-video';
-
 
 const Home = (props) => {
   return (
     <View>
-        <SearchBar />
-        <Text>Yoyoyo</Text>
-       {/* <View style={{height: Dimensions.get('window').height - 400}}> */}
-         {/* <HomeMap /> */}
-       {/* </View>  */}
+      <SearchBar />
+      <Text>Yoynmvcnmnmcmnmnvnmmoyo</Text>
+      {/* <View style={{height: Dimensions.get('window').height - 400}}> */}
+      {/* <HomeMap /> */}
+      {/* </View>  */}
       {/* <Carousel data={dummyData}/> */}
       {/* <Image source={taxiimg} style={{ width: 305, height: 159 }} />  */}
-        <RaspberryStrip />
-      
-      {/*  Bottom Comp*/}
+      <RaspberryStrip />
 
+      {/*  Bottom Comp*/}
     </View>
   );
 };
 
 export default Home;
-
-
